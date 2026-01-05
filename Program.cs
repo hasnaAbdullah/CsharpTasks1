2f98a55dec1701366b13759f08afdd61bc36a1c5
hasna.ak <hasna.ak@MEML2030>
2026-01-04 20:38:49 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CsharpPart1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            float sum = 0;
+            float number;
+            float average = 0;
+            Console.Write("Enter how many numbers: ");
+            count = int.Parse(Console.ReadLine());
+
+            if (count <= 0)
+            {
+                Console.WriteLine("Count must be greater than zero.");
+
+            }
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write("Enter number " + i + ": ");
+                number = float.Parse(Console.ReadLine());
+                sum += number;
+                Console.WriteLine(" the sum =" + sum);
+                Console.WriteLine("Average =" + average);
+                number = float.Parse(Console.ReadLine());
+                average += number;
+                Console.WriteLine(" the average  =" + average);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+}
+   
